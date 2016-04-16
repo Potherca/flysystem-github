32daf4d61e40e93b00e17e370e6a602ad4281c7f
Ben Peachey <potherca@gmail.com>
2016-04-16 21:40:14 +0200

    Adds test for recursive folder functionality of GithubAdapter::listContents()
    - Adds keys to provided data for easy idintifiaction in case of failure
    - Adds JSON file as fixture

--- tests/unit-tests/GithubAdapterTest.php
+++ tests/unit-tests/GithubAdapterTest.php
@@ -1,7 +1,9 @@
 <?php
 
-
 namespace Potherca\Flysystem\Github;
+
+use PHPUnit_Framework_MockObject_Matcher_Parameters;
+use PHPUnit_Framework_MockObject_MockObject as MockObject;
 
 /**
  * Tests for the  GithubAdapter class
@@ -15,10 +17,11 @@ class GithubAdapterTest extends \PHPUnit_Framework_TestCase
 {
     ////////////////////////////////// FIXTURES \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
     const MOCK_FILE_PATH = '/path/to/mock/file';
+    const MOCK_FOLDER_PATH = 'a-directory';
 
     /** @var GithubAdapter  */
     private $adapter;
-    /** @var ApiInterface|\PHPUnit_Framework_MockObject_MockObject */
+    /** @var ApiInterface|MockObject */
     private $mockClient;
 
     /**
@@ -46,13 +49,23 @@ class GithubAdapterTest extends \PHPUnit_Framework_TestCase
      * @param $method
      * @param $apiMethod
      * @param $parameters
+     * @param mixed $returnValue
      */
-    final public function testAdapterShouldPassParameterToClient($method, $apiMethod, $parameters)
+    final public function testAdapterShouldPassParameterToClient($method, $apiMethod, $parameters, $returnValue = null)
     {
-        $mocker = $this->mockClient->expects($this->exactly(1))
-            ->method($apiMethod);
+        if (is_string($returnValue) && is_file(sprintf('%s/../fixtures/%s.json', __DIR__, $returnValue))) {
+            $fixturePath = sprintf('%s/../fixtures/%s.json', __DIR__, $returnValue);
+            $fixture = json_decode(file_get_contents($fixturePath), true);
+            $returnValue = $fixture['tree'];
+        }
 
-        $mocker->getMatcher()->parametersMatcher = new \PHPUnit_Framework_MockObject_Matcher_Parameters($parameters);
+
+        $mocker = $this->mockClient->expects(self::exactly(1))
+            ->method($apiMethod)
+            ->willReturn($returnValue)
+        ;
+
+        $mocker->getMatcher()->parametersMatcher = new PHPUnit_Framework_MockObject_Matcher_Parameters($parameters);
 
         call_user_func_array([$this->adapter, $method], $parameters);
     }
@@ -63,14 +76,17 @@ class GithubAdapterTest extends \PHPUnit_Framework_TestCase
     final public function provideReadMethods()
     {
         return [
-            ['has', 'exists', [self::MOCK_FILE_PATH]],
-            ['read', 'getFileContents', [self::MOCK_FILE_PATH]],
-            ['listContents', 'getTreeMetadata', [self::MOCK_FILE_PATH, true]],
-            ['getMetadata', 'getMetadata', [self::MOCK_FILE_PATH]],
-            ['getSize', 'getMetadata', [self::MOCK_FILE_PATH]],
-            ['getMimetype', 'guessMimeType', [self::MOCK_FILE_PATH]],
-            ['getTimestamp', 'getLastUpdatedTimestamp', [self::MOCK_FILE_PATH]],
-            ['getVisibility', 'getTreeMetadata', [self::MOCK_FILE_PATH, false]],
+            'has' => ['has', 'exists', [self::MOCK_FILE_PATH]],
+            'read' => ['read', 'getFileContents', [self::MOCK_FILE_PATH]],
+            'listContents - File' => ['listContents', 'getTreeMetadata', [self::MOCK_FILE_PATH, false]],
+            'listContents - File - recursive' => ['listContents', 'getTreeMetadata', [self::MOCK_FILE_PATH, true]],
+            'listContents - Folder' => ['listContents', 'getTreeMetadata', [self::MOCK_FOLDER_PATH, false], ''],
+            'listContents - Folder - recursive' => ['listContents', 'getTreeMetadata', [self::MOCK_FOLDER_PATH, true], 'listContents-folder-recursive'],
+            'getMetadata' => ['getMetadata', 'getMetadata', [self::MOCK_FILE_PATH]],
+            'getSize' => ['getSize', 'getMetadata', [self::MOCK_FILE_PATH]],
+            'getMimetype' => ['getMimetype', 'guessMimeType', [self::MOCK_FILE_PATH]],
+            'getTimestamp' => ['getTimestamp', 'getLastUpdatedTimestamp', [self::MOCK_FILE_PATH]],
+            'getVisibility' => ['getVisibility', 'getTreeMetadata', [self::MOCK_FILE_PATH]],
         ];
     }
 }
